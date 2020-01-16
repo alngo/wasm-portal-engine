78615a3e148065e5226a7b27febc3db342565622
alngo <alngo@student.42.fr>
2020-01-16 17:56:17 +0100

    feat(map): Add map exemple in readme.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-#[macro_use]
 extern crate more_asserts;
 extern crate wasm_bindgen;
+extern crate serde_json;
+extern crate sdl2;
 use wasm_bindgen::prelude::*;
 
 mod utils;
+mod map;
 mod sector;
 mod canvas;
 mod player;
--- src/map/mod.rs
+++ src/map/mod.rs
@@ -1,26 +1,25 @@
-use crate::utils::vector::Vec2;
+use std::fs;
 use crate::sector::Sector;
+use crate::player::Player;
+use serde_json::{Result, Value};
+
+pub struct Vertex(f32, f32);
 
 pub struct Map {
-    pub vertexes: Vec<Vec2>,
+    pub vertexes: Vec<Vertex>,
     pub sectors: Vec<Sector>,
+    pub player: Player
 }
 
-impl Default for Map {
-    fn default() -> Self {
-        Self {
-            vertexes: vec![
-                Vec2(0.0, 0.0),
-                Vec2(5.0, 0.0),
-                Vec2(5.0, 5.0),
-                Vec2(0.0, 5.0)
-            ]
-            sectors: vec![Sector {
-                floor: 0.0,
-                ceil: 20.0,
-                vertex: vec![0,1,2,3],
-                neighbors vec![]
-            }]
+impl Map {
+    pub fn load(&mut self, filename: &str) -> Map {
+        let contents = fs::read_to_string(filename)
+            .expect("Something went wrong reading the file");
+        let decode = serde_json::from_str(&contents).unwrap();
+        print!("deserialized: {:?}", decode);
+        Map {
+            vertexes: decode.vertexes,
+            sectors: decode.sectors,
         }
     }
 }
