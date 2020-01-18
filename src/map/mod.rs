def26dd3ab72a4da29e40ca36297655c62f698ac
alngo <alngo@student.42.fr>
2020-01-18 08:24:10 +0000

    feat(map): add test for map.

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -36,7 +36,22 @@ impl Default for Map {
 mod map_tests {
     use super::*;
     #[test]
-    fn it_should_match_default_value() {
+    fn it_should_match_default_values() {
+        let map = Map::default();
+        assert_eq!(map.vertexes.len(), 0);
+        assert_eq!(map.sectors.len(), 0);
+        assert_eq!(map.player.position.0, 0.0);
+        assert_eq!(map.player.position.1, 0.0);
+        assert_eq!(map.player.position.2, 0.0);
+        assert_eq!(map.player.velocity.0, 0.0);
+        assert_eq!(map.player.velocity.1, 0.0);
+        assert_eq!(map.player.velocity.2, 0.0);
+        assert_eq!(map.player.angle, 0.0);
+        assert_eq!(map.player.sector, 0);
+    }
+
+    #[test]
+    fn it_should_match_data_values() {
         let data = r#"
         {
             "vertexes": [
@@ -54,13 +69,29 @@ mod map_tests {
                 }
             ],
             "player": {
-                "position": [1.0, 2,0, 0.0],
-                "velocity": [0.0, 0.0, 0.0],
-                "angle": 0.0,
-                "sector": 0
+                "position": [1.0, 2.0, 3.0],
+                "velocity": [4.0, 5.0, 6.0],
+                "angle": 7.0,
+                "sector": 8
             }
         }"#;
-        let _map = Map::load(data);
-        //Map::untyped_example();
+        let map = match Map::load(data) {
+            Ok(item) => item,
+            Err(_) => Map::default()
+        };
+        assert_eq!(map.vertexes.len(), 4);
+        assert_eq!(map.sectors.len(), 1);
+        assert_eq!(map.sectors[0].floor, 0.0);
+        assert_eq!(map.sectors[0].ceil, 20.0);
+        assert_eq!(map.sectors[0].vertexes_id, [0, 1, 2, 3]);
+        assert_eq!(map.sectors[0].neighbors_id, [-1, -1, -1, -1]);
+        assert_eq!(map.player.position.0, 1.0);
+        assert_eq!(map.player.position.1, 2.0);
+        assert_eq!(map.player.position.2, 3.0);
+        assert_eq!(map.player.velocity.0, 4.0);
+        assert_eq!(map.player.velocity.1, 5.0);
+        assert_eq!(map.player.velocity.2, 6.0);
+        assert_eq!(map.player.angle, 7.0);
+        assert_eq!(map.player.sector, 8);
     }
 }
